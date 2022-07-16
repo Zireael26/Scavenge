7e78598d3fd342cd6c9e8e6edf1dc071e5765313
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2022-07-17 02:36:00 +0530

    Implement handler for commit scavenge message

--- x/scavenge/keeper/msg_server_commit_solution.go
+++ x/scavenge/keeper/msg_server_commit_solution.go
@@ -5,13 +5,26 @@ import (
 
 	"github.com/Zireael26/scavenge/x/scavenge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CommitSolution(goCtx context.Context, msg *types.MsgCommitSolution) (*types.MsgCommitSolutionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// create a new commit from the information in the MsgCommitSolution message
+	var commit = types.Commit{
+		Index:                 msg.SolutionScavengerHash,
+		SolutionHash:          msg.SolutionHash,
+		SolutionScavengerHash: msg.SolutionScavengerHash,
+	}
 
+	// try getting a commit from the store using solution+scavenger hash
+	_, isFound := k.GetCommit(ctx, commit.SolutionScavengerHash)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Commit with that hash already exists")
+	}
+
+	// write commit to the store
+	k.SetCommit(ctx, commit)
 	return &types.MsgCommitSolutionResponse{}, nil
 }
